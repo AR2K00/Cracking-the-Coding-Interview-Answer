2396a1ee1567966ee24b2ceba87eab89302cc6e3
AR2K00 <50752407+AR2K00@users.noreply.github.com>
2020-06-21 16:17:12 -0400

    Update Question1Point2.java

--- Question1Point2.java
+++ Question1Point2.java
@@ -1,9 +1,11 @@
+//check if two strings are a permutation of each other
 import java.lang.reflect.Array;
 import java.util.*;
 public class Question1Point2 {
     public static void main(String[] args) {
         String str = "rat";
         String str2 = "tab";
+        //Converts both strings to arrays and sorts them, then checks if they are the same string after converting back to a string
         char[] arr = str.toCharArray();
         Arrays.sort(arr);
         char[] arr2 = str2.toCharArray();
