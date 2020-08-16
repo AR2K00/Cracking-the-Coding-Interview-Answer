7c65d4a6d6fc4cb148d54796405bbb709c84a625
AR2K00 <50752407+AR2K00@users.noreply.github.com>
2020-08-16 16:18:29 -0400

    Update Question1.1.java

--- Arrays/Question1.1.java
+++ Arrays/Question1.1.java
@@ -1,12 +1,12 @@
 import java.util.*;
-// How to check if a string has all unique characters, using no other data structure
+// Check if a string has all unique characters, using no other data structure
 public class Question1Point1 {
     public static void main(String[] args) {
-        String str = "red";
+        String test = "red";
         boolean check = true;
-        for (int i = 0; i< str.length()-1; i++){
-            for( int j =i+1; j<str.length(); j++){
-                if(str.charAt(i) == str.charAt(j)){
+        for (int i = 0; i< test.length()-1; i++){
+            for( int j =i+1; j<test.length(); j++){
+                if(test.charAt(i) == test.charAt(j)){
                     check = false;
                     break;
                 }
@@ -14,8 +14,6 @@ public class Question1Point1 {
         }
         //check answer
         System.out.println(check);
-        int val = str.charAt(2);
-        System.out.println(val);
         }
     }
 
