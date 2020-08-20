7d55183e597d5d003240cda56c9afda042a2c066
AR2K00 <50752407+AR2K00@users.noreply.github.com>
2020-08-20 05:45:19 -0400

    Update Question14Point3.java

--- Databases/Question14Point3.java
+++ Databases/Question14Point3.java
@@ -1,6 +1,3 @@
-public class Question14Point3 {
-    //UPDATE Requests
-    //Set Status = 'Closed'
-    // Where AptID(Select AptID From Apartments where BuildingID = 11)
-
-}
+//UPDATE Requests
+//Set Status = 'Closed'
+// Where AptID(Select AptID From Apartments where BuildingID = 11)
