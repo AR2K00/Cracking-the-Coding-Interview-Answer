52ccac5e8dd0877a764d45f5dc6d94bb4f5e091f
AR2K00 <50752407+AR2K00@users.noreply.github.com>
2020-08-20 05:56:15 -0400

    Update Question13Point3.java

--- Java/Question13Point3.java
+++ Java/Question13Point3.java
@@ -1,3 +1,1 @@
-public class Question13Point3 {
-    //They are all used in different scenarios: finally is used with try/catch, final is used in classes with variables, methods and classes to keep them immutable, finalize is used with the garbage collector
-}
+//They are all used in different scenarios: finally is used with try/catch, final is used in classes with variables, methods and classes to keep them immutable, finalize is used with the garbage collector
