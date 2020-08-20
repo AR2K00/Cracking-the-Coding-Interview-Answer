10203d73eca92831e1e4544076cf836530caadc9
AR2K00 <50752407+AR2K00@users.noreply.github.com>
2020-08-20 05:57:35 -0400

    Update and rename Question13Point5.java to Question13.5.java

---
+++ Java/Question13.5.java
@@ -0,0 +1,2 @@
+//HashMap has no ordering but it uses O(1) time to search and add, TreeMaps use O(log N) to search and add and are ordered,
+// but LinkedHashMaps use O(1) to search and add and are ordered by insertion
