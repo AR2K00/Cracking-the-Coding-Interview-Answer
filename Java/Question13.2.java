e2cfe7a21bcd160cca8dec83deebf22f937477ab
AR2K00 <50752407+AR2K00@users.noreply.github.com>
2020-08-20 05:55:38 -0400

    Update and rename Question13Point2.java to Question13.2.java

---
+++ Java/Question13.2.java
@@ -0,0 +1,1 @@
+//The finally block can still be executed if we insert a return statement inside of the try. This is because the finally block will still execute if the return is not executed.
--- Java/Question13Point2.java
+++
@@ -1,3 +0,0 @@
-public class Question13Point2 {
-    //The finally block can still be executed if we insert a return statement inside of the try. This is because the finally block will still execute if the return is not executed.
-}
