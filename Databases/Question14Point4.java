11eafee6c80e4c687fa88bf336086237e4a6518c
AR2K00 <50752407+AR2K00@users.noreply.github.com>
2020-08-20 05:46:48 -0400

    Update Question14Point4.java

--- Databases/Question14Point4.java
+++ Databases/Question14Point4.java
@@ -1,4 +1,2 @@
-public class Question14Point4 {
-    //Inner Join gives only the data that fits the requerments, Outer has everything in inner and also has some data that doesn't have matches in other table
-    //Outer is split up into left, where it has all daya from the left, the right join, where data is from right, andthe full, which combines both
-}
+//Inner Join gives only the data that fits the requerments, Outer has everything in inner and also has some data that doesn't have matches in other table
+//Outer is split up into left, where it has all daya from the left, the right join, where data is from right, andthe full, which combines both
