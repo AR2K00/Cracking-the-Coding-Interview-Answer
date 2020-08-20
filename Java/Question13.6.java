8f7ffe750a3b8c18d3f71d4d0f4eaa31438000d0
AR2K00 <50752407+AR2K00@users.noreply.github.com>
2020-08-20 05:58:14 -0400

    Update and rename Question13Point6.java to Question13.6.java

---
+++ Java/Question13.6.java
@@ -0,0 +1,2 @@
+//Allows for the reflective info aboutclasses adn objects to be viewed. It cna also be used to make new instances and viewing/ manipulating object fields.
+// It is useful with debugging and testing.
--- Java/Question13Point6.java
+++
@@ -1,4 +0,0 @@
-public class Question13Point6 {
-    //Allows for the reflective info aboutclasses adn objects to be viewed. It cna also be used to make new instances and viewing/ manipulating object fields.
-    // It is useful with debugging and testing.
-}
