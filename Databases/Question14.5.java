a290e4ee9fed9dbf3636600d7f15f46126498ed1
AR2K00 <50752407+AR2K00@users.noreply.github.com>
2020-08-20 05:48:01 -0400

    Update and rename Question14Point5.java to Question14.5.java

---
+++ Databases/Question14.5.java
@@ -0,0 +1,2 @@
+//Denormalization is when redundant data is added to avoid Joins. Updating and adding is going to be less efficient and more storage is taken up,
+//but getting data is faster with less joins
--- Databases/Question14Point5.java
+++
@@ -1,4 +0,0 @@
-public class Question14Point5 {
-    //Denormalization is when redundant data is added to avoid Joins. Updating and adding is going to be less efficient and more storage is taken up,
-    // but getting data is faster with less joins
-}
